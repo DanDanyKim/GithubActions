9321443c5ca8dcbb0b766bfb6857aefc87cb6d48
DanDanyKim <137750820+DanDanyKim@users.noreply.github.com>
2023-07-06 15:46:59 +0900

    Update slack.sh

--- gradle/scripts/actions/slack.sh
+++ gradle/scripts/actions/slack.sh
@@ -1,9 +1,9 @@
 distributors=("김단희" "김단희")
 distributor_sequence=$(cat ./distributor_sequence)
-today_distributor=$distributors[${distributor_sequence}]
+today_distributor=${distributors[${distributor_sequence}]}
 echo $today_distributor
 
-payload="{\"text\": \"이번 배포 담당자는 @$(today_distributor)_개발 님 입니다\"}"
+payload="{\"text\": \"이번 배포 담당자는 @${today_distributor}_개발 님 입니다\"}"
 
 slack_reminder_webhook=$1
 curl -X POST -H 'Content-type: application/json' --data "$payload" $slack_reminder_webhook
