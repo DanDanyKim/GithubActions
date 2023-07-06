8cacd92eae927f37cb999bbcad1516376cef0bdf
DanDanyKim <137750820+DanDanyKim@users.noreply.github.com>
2023-07-06 17:32:43 +0900

    Update distributor_reminder.sh

--- gradle/scripts/actions/distributor_reminder.sh
+++ gradle/scripts/actions/distributor_reminder.sh
@@ -1,10 +1,10 @@
-distributors=("김다슬" "김단희" "서주홍" "유건우" "이아연A" "이주영" "주재호" "최병문" "최은정")
+distributors=$1
 distributor_sequence=$(cat gradle/scripts/actions/distributor_sequence)
 today_distributor=${distributors[${distributor_sequence}]}
 
 payload="{\"text\": \"이번 배포 담당자는 @${today_distributor}_개발 님 입니다\"}"
 
-slack_reminder_webhook=$1
+slack_reminder_webhook=$2
 curl -X POST -H 'Content-type: application/json' --data "$payload" $slack_reminder_webhook
 
 distributor_num=${#distributors[@]}
