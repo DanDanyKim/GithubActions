a7486519e404e5100fb786613e512185c6cf39e2
DanDanyKim <137750820+DanDanyKim@users.noreply.github.com>
2023-07-06 14:45:48 +0900

    Create slack.sh

---
+++ gradle/scripts/actions/slack.sh
@@ -0,0 +1,4 @@
+distributors=("김다슬","김단희")
+payload="{\"text\": \"이번 배포 담당자는 @$distributors[0] 입니다\"}"
+slack_reminder_webhook="https://hooks.slack.com/services/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"
+curl -X POST -H 'Content-type: application/json' --data "$payload" $slack_reminder_webhook
